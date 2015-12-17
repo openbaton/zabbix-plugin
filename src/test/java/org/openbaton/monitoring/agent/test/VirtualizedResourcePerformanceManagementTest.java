c0226126dec57ac515e2073cd93a6fae49edcdec
Marcello Monachesi <marcello.monachesi@fokus.fraunhofer.de>
2015-12-17 15:03:54 +0100

    Added header and fixed tests

--- src/test/java/org/openbaton/monitoring/agent/test/VirtualizedResourcePerformanceManagementTest.java
+++ src/test/java/org/openbaton/monitoring/agent/test/VirtualizedResourcePerformanceManagementTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015 Fraunhofer FOKUS
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openbaton.monitoring.agent.test;
 
 import org.junit.After;
@@ -46,7 +61,7 @@ public class VirtualizedResourcePerformanceManagementTest {
     @Test
     public void createAndDeleteThresholdTest() throws MonitoringException {
         ObjectSelection objectSelector= getObjectSelector();
-        ThresholdDetails thresholdDetails= new ThresholdDetails("last(0)","0","=");
+        ThresholdDetails thresholdDetails= new ThresholdDetails("last(0)","=",PerceivedSeverity.CRITICAL,"0","|");
         thresholdDetails.setPerceivedSeverity(PerceivedSeverity.CRITICAL);
 
         String thresholdId = zabbixMonitoringAgent.createThreshold(objectSelector,"net.tcp.listen[5001]",null,thresholdDetails);
