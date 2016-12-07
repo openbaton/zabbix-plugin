0e4aa3721bb5c9ac8223baafd597a7b8efe103c2
Philipp Emanuel Kuhnz <philipp.emanuel.kuhnz@fokus.fraunhofer.de>
2016-12-07 16:50:51 +0100

    Imp: updated action model to 3.0 schema

---
+++ src/main/java/org/openbaton/monitoring/agent/zabbix/api/Filter.java
@@ -0,0 +1,50 @@
+/*
+* Copyright (c) 2015-2016 Fraunhofer FOKUS
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.openbaton.monitoring.agent.zabbix.api;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pku on 07.12.16.
+ */
+public class Filter {
+  @SerializedName("conditions")
+  @Expose
+  private List<Condition> conditions = new ArrayList<Condition>();
+  @SerializedName("evaltype")
+  @Expose
+  private Integer evaltype;
+
+  public List<Condition> getConditions() {
+    return conditions;
+  }
+
+  public void setConditions(List<Condition> conditions) {
+    this.conditions = conditions;
+  }
+
+  public Integer getEvaltype() {
+    return evaltype;
+  }
+
+  public void setEvaltype(Integer evaltype) {
+    this.evaltype = evaltype;
+  }
+}
